cace73d40ce5cb113c1e934cfc204db076f3648c
mahmoud09sakr <mahmoudroute199@gmail.com>
2025-02-22 11:09:57 +0200

    update auth.js

--- src/midlleware/auth.js
+++ src/midlleware/auth.js
@@ -25,13 +25,12 @@ export const auth = async (req, res, next) => {
         }
         let decoded = jwt.verify(token, signature)
         if (!decoded) {
-            res.json({ message: 'un autharized' })
             throw new AppError('un autharized', 403)
         }
         let user = await userModel.findById(decoded.id)
         req.user = user
         next()
     } catch (error) {
-      throw new AppError('un autharized', 403)
+        res.json({ message: 'un autharized' })
     }
 }
