c402da6ebd7bbeb8126c68edcbd71420ee991b1d
mahmoud09sakr <mahmoudroute199@gmail.com>
2025-02-23 11:27:46 +0200

    remove logs

--- src/utilts/multer.js
+++ src/utilts/multer.js
@@ -12,15 +12,11 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET_KYE
 });
 
-console.log('Cloudinary Config:', {
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_CLOUD_API_KYE,
-    api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET_KYE ? 'Set' : 'Not Set'
-});
+
 const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
+    limits: { fileSize: 10 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
         console.log('Filtering file:', file.originalname, file.mimetype);
         if (['image/jpeg', 'image/png', 'image/jpg'].includes(file.mimetype)) {
@@ -43,13 +39,15 @@ const safeUpload = (uploadFn) => (req, res, next) => {
 };
 export const uploadToCloudinary = (isRequired = true) => async (req, res, next) => {
     try {
+        //TODO: matnsa4 remove cloudinary comments
         console.log('Files before upload:', req.files);
         console.log('File (single) before upload:', req.file);
         console.log('Body before upload:', req.body);
+        //TODO: matnsa4 remove cloudinary comments
         if (!req.files && !req.file && isRequired) {
             return next(new AppError('File upload is required', 400));
         }
-        if (req.file) { // Single file upload
+        if (req.file) {
             const promise = new Promise((resolve, reject) => {
                 const stream = cloudinary.uploader.upload_stream(
                     { resource_type: 'auto', timeout: 15000 },
